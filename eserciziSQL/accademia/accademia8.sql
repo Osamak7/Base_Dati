7dde855e5409025ac1a8d4ef33349603b5f6897b
Osamak77 <hliwaoussama7@gmail.com>
2024-10-25 13:45:20 +0200

    add ac8

--- eserciziSQL/accademia/accademia8.sql
+++ eserciziSQL/accademia/accademia8.sql
@@ -1,11 +1,13 @@
 #1
-select  p.id, p.nome, p.cognome
-from Persona p 
-join Assenza ass on ass.persona = p.id 
-full outer join attivitaProgetto atp on  atp.persona = ass.persona
-full outer join attivitaNonProgettuale anp on anp.persona = ass.persona
-where atp.persona is null and anp.persona is null 
-order by p.id asc
+select distinct p.id , p.nome , p.cognome
+from persona p 
+join assenza ass on ass.persona = p.id
+full join Attivitanonprogettuale anp on anp.giorno = ass.giorno 
+full join AttivitaProgetto atp on atp.giorno = ass.giorno 
+group by p.id , p.nome, p.cognome
+having count (anp.giorno) = 0
+and count (atp.giorno)= 0
+
 
 #2
 select p.id, p.nome, p.cognome 
